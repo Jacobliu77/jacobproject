ee9e7243818dbde0e748d703b0b7b7feafbf7412
e <751232478@qq.com>
2019-12-18 22:18:06 +0800

    动态自动校验未完整-第一次提交

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -2,11 +2,21 @@
   <div class="login">
     <el-card class="box-card">
       <img src="../../assets/img/logo_index.png" alt />
-      <el-input v-model="formdata.tel" placeholder="请输入手机号码" class="tel"></el-input>
-      <el-input v-model="formdata.code" placeholder="请输入验证码" class="code"></el-input>
-      <el-button plain style="float:right">发送验证码</el-button>
-      <el-checkbox v-model="formdata.checked" class="check">我已阅读用户协议且同意上述条款</el-checkbox>
-      <el-button type="primary" class="submit">登录</el-button>
+      <el-form ref="myForm" :model="formdata" :rules="loginRules">
+        <el-form-item prop="tel">
+          <el-input v-model="formdata.tel" placeholder="请输入手机号码" class="tel"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="formdata.code" placeholder="请输入验证码" class="code"></el-input>
+          <el-button plain style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop="agree">
+          <el-checkbox v-model="formdata.check" class="check">我已阅读用户协议且同意上述条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="login" type="primary" class="submit">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -14,12 +24,35 @@
 <script>
 export default {
   data () {
+    let validator = function (rule, value, callBack) {
+      value ? callBack() : callBack(new Error('您必须同意无条件被我们蒙骗')) // 炫技模式
+    }
     return {
       formdata: {
         tel: '',
         code: '',
-        checked: false
+        check: false
+      },
+      loginRules: {
+        tel: [
+          { required: true, message: '请输入您的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }
+        ],
+        code: [
+          { required: true, message: '请输入您的验证码' },
+          { pattern: /^\d{6}$/, message: '验证码为6位数字' }
+        ],
+        agree: [{ validator }]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          console.log('校验成功')
+        }
+      })
     }
   }
 }
@@ -41,20 +74,15 @@ export default {
       height: 40px;
       margin-bottom: 20px;
     }
-    .tel {
-      margin-bottom: 20px;
-    }
     .code {
       float: left;
       width: 70%;
     }
-    .check{
-        margin-top: 20px;
-         float: left;
+    .check {
+      float: left;
     }
-    .submit{
-        margin-top: 20px;
-        width: 100%;
+    .submit {
+      width: 100%;
     }
   }
 }
