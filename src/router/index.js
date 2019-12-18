e11a6ffc0c686154fb7588f2cad0a823031aa64a
e <751232478@qq.com>
2019-12-18 21:43:03 +0800

    头条PC-登录模块-页面布局及样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home/Home.vue'
+import login from '../views/login/login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/home',
     name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: login
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   //按需加载解决首屏加载慢的问题；
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="login">
+    <el-card class="box-card">
+      <img src="../../assets/img/logo_index.png" alt />
+      <el-input v-model="formdata.tel" placeholder="请输入手机号码" class="tel"></el-input>
+      <el-input v-model="formdata.code" placeholder="请输入验证码" class="code"></el-input>
+      <el-button plain style="float:right">发送验证码</el-button>
+      <el-checkbox v-model="formdata.checked" class="check">我已阅读用户协议且同意上述条款</el-checkbox>
+      <el-button type="primary" class="submit">登录</el-button>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formdata: {
+        tel: '',
+        code: '',
+        checked: false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.login {
+  background-image: url("../../assets/img/bgc.jpg");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box-card {
+    width: 440px;
+    height: 320px;
+    opacity: 0.9;
+    img {
+      height: 40px;
+      margin-bottom: 20px;
+    }
+    .tel {
+      margin-bottom: 20px;
+    }
+    .code {
+      float: left;
+      width: 70%;
+    }
+    .check{
+        margin-top: 20px;
+         float: left;
+    }
+    .submit{
+        margin-top: 20px;
+        width: 100%;
+    }
+  }
+}
+</style>
